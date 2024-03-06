bc8adacdd69480cfc6de3dd3c068ffa62b36469e
Taffarel Oliveira <115137992+kirakernel@users.noreply.github.com>
2024-03-06 13:05:23 +0000

    `configuration/views/assets`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // $production: {
+  //   routeRules: {
+  //     '/**': { isr: true }
+  //   }
+  // },
+  // $development: {
+  //   //
+  // }
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    superSecretThing: '123456',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api',
+      users: '/api/users',
+    }
+  },
+  vue: {
+    propsDestructure: true,
+  },
 })
